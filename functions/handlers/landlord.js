4fd09e2fc588cd93dfed7097026cb409288e7adc
Kate <gd18811370228@outlook.com>
2020-02-20 12:35:38 -0800

    add three functions- get verified/unverified users; delete user

--- functions/handlers/landlord.js
+++ functions/handlers/landlord.js
@@ -89,6 +89,54 @@ exports.suspendWorker = (req,res) => {
     console.error(err)
     return res.status(400).json({error: `Unable to suspend ${req.params.worker_email}`})
   })
-
-
 }
+
+exports.getVerifiedUsers = (req, res) => {
+  db
+  .collection('users')
+  .orderBy('created_at', 'desc')
+  .where(('verifed_tenant', '==', true) || ('verifed_tenant', '==', true))
+  .get()
+    .then( data => {
+      let users = [];
+      data.forEach((doc) => {
+        users.push({
+        address: doc.data().address,
+        created_at:  doc.data().created_at,
+        email: doc.data().email,
+        user_id:  doc.data().user_id,
+        verified_ll: doc.data().verified_ll,
+        verified_tenant: doc.data().verified_tenant,
+        verified_worker: doc.data().verified_worker,
+        full_name: doc.data().full_name
+      });
+    });
+      return res.json(users);
+    })
+    .catch(err => console.error(err));
+};
+
+exports.getUnverifiedUsers = (req, res) => {
+  db
+  .collection('users')
+  .orderBy('created_at', 'desc')
+  .where(('verifed_tenant', '==', false) && ('verifed_tenant', '==', false) && ('verifed_ll', '==', false))
+  .get()
+    .then( data => {
+      let users = [];
+      data.forEach((doc) => {
+        users.push({
+        address: doc.data().address,
+        created_at:  doc.data().created_at,
+        email: doc.data().email,
+        user_id:  doc.data().user_id,
+        verified_ll: doc.data().verified_ll,
+        verified_tenant: doc.data().verified_tenant,
+        verified_worker: doc.data().verified_worker,
+        full_name: doc.data().full_name
+      });
+    });
+      return res.json(users);
+    })
+    .catch(err => console.error(err));
+};
--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@ const cors = require('cors');
 app.use(cors());
 
 const { getAllTickets, postOneTicket, getAssignedTickets, getUnnassignedTickets, getTenantTickets, deleteTicket } = require('./handlers/tickets');
-const{signup, login, editAccount, viewProfile} = require('./handlers/users');
-const { verifyWorker, verifyTenant, suspendTenant, suspendWorker } = require('./handlers/landlord')
+const{signup, login, editAccount, viewProfile, deleteAccount} = require('./handlers/users');
+const { verifyWorker, verifyTenant, suspendTenant, suspendWorker, getVerifiedUsers, getUnverifiedUsers } = require('./handlers/landlord')
 const { closeTicket, assignTicket, unassignTicket } = require('./handlers/worker')
 
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -27,7 +27,7 @@ app.post('/signup', signup);
 app.post('/login', login);
 app.post('/edit_account', FBAuth, editAccount);
 app.get('/view_profile',FBAuth, viewProfile);
-//app.post('/delete_account', FBAuth, deleteAccount)
+app.delete('/delete_account', FBAuth, deleteAccount)
 
 //Tenant Routes
 app.get('/tenant_tickets', FBAuth, getTenantTickets);
@@ -44,8 +44,8 @@ app.post('/verify_tenant/:tenant_email', FBAuthLL, verifyTenant);
 app.post('/verify_worker/:worker_email', FBAuthLL, verifyWorker);
 app.post('/suspend_tenant/:tenant_email', FBAuthLL, suspendTenant);
 app.post('/suspend_worker/:worker_email', FBAuthLL, suspendWorker);
-//app.get('/verified_users', FBAuthLL, getVerifiedUsers);
-//app.get('/unverified_users', FBAuthLL, getUnverifiedUsers);
+app.get('/verified_users', FBAuthLL, getVerifiedUsers);
+app.get('/unverified_users', FBAuthLL, getUnverifiedUsers);
 // app.delete('/delete_user', FBAuthLL, deleteUser);
 //app.delete('/delete_ticket/:ticket_id', FBAuthLL, deleteTicket);
 // app.post('/assign_ticket/:ticket_id', FBAuthLL, assignTicket);
